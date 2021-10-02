723c34082f67b6a66282b23444ad8aa3004260af
said <saiddemirhan@gmail.com>
2021-10-02 17:41:05 +0200

    show

---
+++ ders6_nesneler/tvShow/script.js
@@ -0,0 +1,31 @@
+var showListe = [];
+
+//todo json doysasindan veri getirme asamalari
+
+fetch("./tv-shows.json")
+  .then((cevap) => cevap.json())
+  .then((veri) => {
+    showListe = veri;
+
+    showIzle(showListe); //method call
+  });
+
+//method olustur
+function showIzle(showListe) {
+  //!https://getbootstrap.com/docs/4.0/components/card/
+  var liste = document.querySelector(".liste");
+  showListe.forEach((a) => {
+    liste.innerHTML =
+      liste.innerHTML +
+      `<div class="card col-md-3">
+    <img class="card-img-top" src=${
+      a.show.image ? a.show.image.medium : ""
+    } alt="">
+      <h5>${a.show.name}</h5>
+      <a href="${
+        a.show.url
+      }" target="_blank" class="btn btn-success">Detaylar</a>
+    </div>
+    </div>`;
+  });
+}
